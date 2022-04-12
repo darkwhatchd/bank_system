a94c48871853980c3042f6afe3ebf3bfc7fb4924
Daniel Santos <danielevani97@gmail.com>
2022-04-12 12:58:11 -0300

    Authentication with devise to User. Permit parameters name and document on sign up or account update.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   validates :name, presence: true
-  validates :document, presence: true
+  validates :document, presence: true, uniqueness: { case_sensitive: false }
 end
