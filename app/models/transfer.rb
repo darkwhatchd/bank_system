3a3a673239477020d5228efa5889c465c15cef9e
Daniel Santos <danielevani97@gmail.com>
2022-04-12 22:11:03 -0300

    Custom validator to not permit account == destiny_account

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -3,7 +3,7 @@ class Transfer < ApplicationRecord
   belongs_to :destiny_account, class_name: "Account"
 
   validates :account, presence: true
-  validates :destiny_account, presence: true
+  validates :destiny_account, presence: true, different_user: true
   validates :transfer_date, presence: true
   validates :value, presence: true, numericality: { greater_than: 0 }
 end
