454c472ccf722aa5eef048ef1e3fdb6aeb8552a5
Daniel Santos <danielevani97@gmail.com>
2022-04-12 23:52:27 -0300

    Service adjust

--- app/services/tax_calculate_service.rb
+++ app/services/tax_calculate_service.rb
@@ -8,7 +8,7 @@ class TaxCalculateService
       @record.value += 10
     end
 
-    if @record.transfer_date.hour >= 18 || @record.transfer_date.hour < 9
+    if (@record.transfer_date.hour >= 18 || @record.transfer_date.hour < 9) && !@record.transfer_date.saturday? && !@record.transfer_date.sunday?
       @record.value += 7
     elsif @record.transfer_date.saturday? || @record.transfer_date.sunday?
       @record.value += 7
