84be6b321231de2f7ef4606c84922321d8f96e2a
Daniel Santos <danielevani97@gmail.com>
2022-04-12 23:47:59 -0300

    Service to calculate transfer tax

---
+++ app/services/tax_calculate_service.rb
@@ -0,0 +1,19 @@
+class TaxCalculateService
+  def initialize(record)
+    @record = record
+  end
+
+  def call
+    if @record.value > 1000
+      @record.value += 10
+    end
+
+    if @record.transfer_date.hour >= 18 || @record.transfer_date.hour < 9
+      @record.value += 7
+    elsif @record.transfer_date.saturday? || @record.transfer_date.sunday?
+      @record.value += 7
+    else
+      @record.value += 5
+    end
+  end
+end
