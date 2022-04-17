0078541d389dc01db832381d11eebf5cb3d8b9d0
Daniel Santos <danielevani97@gmail.com>
2022-04-17 11:04:38 -0300

    SelectOperationsService updated to select transfers and sort by created_at

--- app/services/select_operations_service.rb
+++ app/services/select_operations_service.rb
@@ -4,8 +4,20 @@ class SelectOperationsService
   end
 
   def call
+    @operations = select_operations
+    @transfers = select_transfers
+    @transfers_operations = @operations.concat(@transfers).sort_by { |element| element.created_at }.reverse
+  end
+
+  def select_operations
     @operations = @statement.account.operations.select do |op|
       op.operation_date >= @statement.start_date && op.operation_date <= @statement.finish_date
+    end
+  end
+
+  def select_transfers
+    @transfers = @statement.account.transfers.select do |op|
+      op.transfer_date >= @statement.start_date && op.transfer_date <= @statement.finish_date
     end
   end
 end
